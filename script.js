da720394a5f5c4c8f16802f9ea4c09c9b6aa18e0
prasadmagi <magiprasad@gmail.com>
2022-07-09 19:42:59 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Javascript code for the right-side menu bar in small devices
+
+var navlinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navlinks.style.right = "0";
+}
+function hideMenu() {
+    navlinks.style.right = "-200px";
+}
+
